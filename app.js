78830054a9a14a95c6dd91da683b0b84d1aa394b
ClaytonForbes <101clay@live.com>
2023-03-15 17:52:18 -0400

    navBar

--- app.js
+++ app.js
@@ -10,20 +10,18 @@ const test =document.getElementById("test");
 
 // show the navigation tabs
 const show = () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
  nav.style.display= "block";
  test.style.width="390px";
  
  closed.style.display ="block";
-=======
+
 nav.style.display= "block";
 closed.style.display ="block";
->>>>>>> origin
-=======
+
 nav.style.display= "block";
 closed.style.display ="block";
->>>>>>> 78a6cc658d16ee0d485dd85400c7b157a97ed601
+
 }
 
 //close the navagation tab
@@ -33,14 +31,5 @@ const noshow = () => {
 }
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// call the navigaton functions
-=======
-// call the navagaton functions
->>>>>>> origin
-=======
-// call the navagaton functions
->>>>>>> 78a6cc658d16ee0d485dd85400c7b157a97ed601
 ele.onclick = show;
 closed.addEventListener("click", noshow);
